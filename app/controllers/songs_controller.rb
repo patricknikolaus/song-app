a1e80098d4b1a5cd17e64cf4df112cbf20b56e02
Patrick <pdn6991@gmail.com>
2021-06-20 13:10:02 -0400

    Added delete route and controller method

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -19,4 +19,23 @@ class SongsController < ApplicationController
       song.save
     render json: song.as_json
   end
+
+  def update
+    song = Song.find_by(id: params[:id])
+    song.title = params[:title] || song.title
+    song.album = params[:album] || song.album
+    song.artist = params[:artist] || song.artist
+    song.year = params[:year] || song.year
+    song.save
+    render json: song.as_json
+  end
+
+  def destroy
+    song = Song.find_by(id: params[:id])
+    song.destroy
+    render json: {
+      message: "you just deleted this song",
+      song: song.as_json
+    }
+  end
 end
